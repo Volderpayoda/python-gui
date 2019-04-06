32d0d9cb814d8ff06eade4d99903d4bd756591f5
Volderpayoda <volderpayoda@gmail.com>
2019-04-06 12:18:06 -0300

    Importación de datos exitosa
    Ya se pueden importar datos, extraer clases y atributos.

--- import_csv.py
+++ import_csv.py
@@ -3,5 +3,22 @@ import pandas as pd
 
 path = 'datasets/iris.csv'
 
-df = pd.read_csv(filepath_or_buffer = path, sep = ',')
-print(df)
+# Obtener el conjunto de datos
+data = pd.read_csv(filepath_or_buffer = path, sep = ',')
+
+# Obtener el conjunto de atributos
+attributes = data.columns
+
+# Obtener el conjunto de clases
+length = attributes.size - 1
+classes = data[attributes[length]].unique()
+
+# Eliminar la columna de clase de la lista de atributos
+attributes = attributes.drop(labels = [attributes[length]])
+
+print("Los datos son: ")
+print(data)
+print("Los atributos son: ")
+print(attributes)
+print("Las clases son: ")
+print(classes)
