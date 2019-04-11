a2fef6b61f252deb59601c67e616927e801fffad
Volderpayoda <volderpayoda@gmail.com>
2019-04-11 14:36:41 -0300

    Comenzando a implementar funcionalidades

--- packages/c45.py
+++ packages/c45.py
@@ -1,57 +1,68 @@
 import numpy as np
 import pandas as pd
 
-def impurityEval1(Data):
+def impurityEval1(data):
     # To Do
+    return False
 
-def impurityCalc(Data,Attributes): # calcular-impurezas
+def impurityCalc(data,attributes): # calcular-impurezas
     # To Do
+    return False
 
-def selectAg(p0,p,Attributes):
+def selectAg(p0,p,attributes):
     # To Do
+    return False
 
 def impurityEval2():
     # To Do
+    return False
 
 def leafNode(cj):
     # To Do
+    return False
 
 def createBranch(valueAg):
     # To Do    
+    return False
 
-def decisionNode(Tree, valueAg):
+def decisionNode(tree, valueAg):
     # To Do
+    return False
 
-def partitionD(Data, valueAg, g):
+def partitionD(data, valueAg, g):
     # To Do
+    return False
 
-def frequentClass(Data):
+def frequentClass(data):
     # To Do
+    return False
 
-def sameClassC(Data):
-    # To Do
+def sameClassC(data, classcolumn):
+    if data[classcolumn].nunique() == 1:
+        return True
+    return False
+    
 
-def decisionTree(Data, Attributes, Tree, threshold):
-    # To Do 
-    cj = frequentClass(Data)
-    if sameClassC(Data):
+def decisiontree(data, attributes, classes, classcolumn, tree, threshold):
+    cj = frequentClass(data)
+    if sameClassC(data, classcolumn):
         leafNode(cj)
-    elif Attributes.length() == 0:
+    elif attributes.length() == 0:
         leafNode(cj)
     else:
-        p0 = impurityEval1(Data)
+        p0 = impurityEval1(data)
         p = []
-        p = impurityCalc(Attributes, Data)
-        g = selectAg(p0, p, Attributes)
+        p = impurityCalc(attributes, data)
+        g = selectAg(p0, p, attributes)
         if (p0 - p[g]) < threshold:
-            cj = frequentClass(Data)
+            cj = frequentClass(data)
             leafNode(cj)
         else:
-            decisionNode(Tree, Attributes[g])
-            Dsubsets = partitionD(Data, Attributes[g], g)
+            decisionNode(tree, attributes[g])
+            Dsubsets = partitionD(data, attributes[g], g)
             j = 0
             for d in Dsubsets:
                 if d.length() != 0:
-                    createBranch(Attributes[g])
+                    createBranch(attributes[g])
                     j += 1
-                    decisionTree(Dsubsets[j], A-A[g], Tree[j])
+                    decisiontree(Dsubsets[j], A-A[g], tree[j])
