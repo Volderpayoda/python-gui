7b5d3fc1420dc9446efd55015672be32ceea2e23
Volderpayoda <volderpayoda@gmail.com>
2019-05-02 21:21:27 -0300

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -5,6 +5,8 @@ from PyQt5.QtCore import QObject, pyqtSlot
 from views.mainwindow import Ui_MainWindow
 from models.model import Model
 from packages.importer import *
+from packages.c45 import *
+import packages.binaryTree as bt
 import sys
 
 class MainWindowUIClass(Ui_MainWindow):
@@ -19,9 +21,16 @@ class MainWindowUIClass(Ui_MainWindow):
         # Configuración de la interfaz gráfica sobre el marco de la ventana
         super().setupUi(MW)
 
+        # Ajustar geometría de elementos de pantalla
+        self.separator.setFixedWidth(65)
+        self.lineTerminator.setFixedWidth(65)
+        self.testPer.setFixedWidth(65)
+        
         # Conexión de eventos con funciones
         self.browseButton.clicked.connect(self.browseSlot)
         self.classifyButton.clicked.connect(self.classifySlot)
+        self.confirmButton.clicked.connect(self.confirmSlot)
+        self.discardButton.clicked.connect(self.discardSlot)
 
     def debugPrint(self, msg):
         # Imprime un mensaje en la ventana de debug
@@ -29,23 +38,34 @@ class MainWindowUIClass(Ui_MainWindow):
 
     def setFile(self, fileName):
         # Verifica que el archivo sea valido y lo deja seleccionado. Caso contrario informa al usuario
-        flag, msg = self.model.setModel(fileName)
-        if not flag:
-            self.warningBox(msg)
-        self.debugPrint("Archivo seleccionado: {0}".format(fileName))
-        self.trainingLineEdit(fileName)
+        if not self.model.isValidFile(fileName):
+            warningBox('El archivo seleccionado no existe o no se puede acceder.')
+            return 
+        if not self.model.isCsv(fileName):
+            self.warningBox('El archivo seleccionado no es de tipo CSV.')
+            return
+        problem = import_csv(fileName)
+        if not self.model.isValidProblem(problem):
+            self.warningBox('El conjunto de datos seleccionado debe contener solo 2 atributos.')
+            return
+        self.debugPrint('Archivo seleccionado: {0}'.format(fileName))
+        self.trainingLineEdit.setText(fileName)
+        self.model.setFileName(fileName)
+        self.model.setProblem(problem)
+        self.classifyButton.setEnabled(True)
 
     def warningBox(self, text):
         msg = QtWidgets.QMessageBox()
         msg.setIcon(QtWidgets.QMessageBox.Warning)
         msg.setText(text)
+        msg.setWindowTitle('Advertencia')
         msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msg.exec()
 
     # Slots del programa para el entrenamiento
     def browseSlot(self):
         # Llamado cuando el usuario presiona el Boton Examinar
-        self.debugPrint("Botón examinar presionado")
+        self.debugPrint('Botón examinar presionado')
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName()
         if fileName: 
             self.setFile(fileName)
@@ -56,10 +76,35 @@ class MainWindowUIClass(Ui_MainWindow):
         # TODO: En este punto se llamaría al algoritmo de clasificación
         self.debugPrint("Botón clasificar presionado")
         problem = self.model.getProblem()
-        for key in problem:
-            self.debugPrint("{0}: ".format(key))
-            self.debugPrint(str(problem[key]))
+        tree = decisionTree(problem.data, problem.attributes, problem.classes, problem.classcolumn, bt.BinaryTree(), 0.1)
+        self.debugPrint(str(tree))
+
+    def confirmSlot(self):
+        self.debugPrint('Boton confirmar presionado')
+        # Controla que se haya ingresado el porcentaje de elementos para prueba
+        if self.testPer.text() == '':
+            self.warningBox('Debe indicar un porcentaje de elementos que serán usados para la prueba')
+            return
+        else:
+            testPer = self.testPer.text()
+        # Asigna los valores para el separador y el fin de línea 
+        if self.separator.text() == '':
+            separator = ','
+        else:
+            separator = self.separator.text()
+        
+        if self.lineTerminator.text() == '':
+            lineTerminator = '\n'
+        else:
+            lineTerminator = self.lineTerminator.text()
+        # self.debugPrint('Separador: {0} \n Fin de línea: {1} \n Porcentaje: {2}'.format(separator, lineTerminator, testPer)
+        self.confirmButton.setEnabled(False)
+        self.configurationFrame.setEnabled(False)
     
+    def discardSlot(self):
+        self.confirmButton.setEnabled(True)
+        self.configurationFrame.setEnabled(True)     
+
     # Slots del programa para la prueba
     def testBrowseSlot(self):
         # TODO
