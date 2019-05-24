b9d429de3f7c160558a4a2c138801b8ea630286f
Volderpayoda <volderpayoda@gmail.com>
2019-05-24 11:50:23 -0300

    Update treePlotter.py

--- packages/treePlotter.py
+++ packages/treePlotter.py
@@ -1,15 +1,13 @@
 import time
 from os import path
-def plotTree(tree):
+def plotTree(tree, libPath):
     f = open(path.abspath('./views/template.html'), 'r')
     s = f.read()
     s = s.replace('treeDataJson', tree)
+    s = s.replace('libPath', libPath)
     temp = str(time.time()) + '.html'
     filePath = path.abspath(path.join('./temp', temp))
     o = open(filePath, 'a')
     o.write(s)
     o.close()
     return filePath
-
-if __name__ == '__main__':
-    print(plotTree('Éxito'))
